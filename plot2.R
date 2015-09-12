6e8697bbfab0d4f0694cf149e369420b6811b82c
Sarojini <nsarojinidv@gmail.com>
2015-09-12 11:17:10 +0800

    plot2

---
+++ plot2.R
@@ -0,0 +1,19 @@
+filename <- "household_power_consumption.txt"
+df <- read.table(filename,
+                 header=TRUE,
+                 sep=";",
+                 colClasses=c("character", "character", rep("numeric",7)),
+                 na="?")
+# convert date and time variables to Date/Time class
+df$Time <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
+df$Date <- as.Date(df$Date, "%d/%m/%Y")
+# only use data from the dates 2007-02-01 and 2007-02-02
+dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+df <- subset(df, Date %in% dates)  
+png("plot2.png", width=480, height=480)
+
+plot(df$Time, df$Global_active_power,
+     type="l",
+     xlab="",
+     ylab="Global Active Power (kilowatts)")
+dev.off()
